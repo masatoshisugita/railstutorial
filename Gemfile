b82ba893a7c073ec2729a300bc3a2b861aa51237
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2018-10-27 16:16:17 +0900

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap-sass', '3.3.7'
 
 gem 'bcrypt',         '3.1.12'
+
+gem "jquery-rails"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+      @user = User.new(user_params)
     if @user.save
+       log_in @user
        flash[:success] = "Welcome to the Sample App!"
        redirect_to @user
     else
